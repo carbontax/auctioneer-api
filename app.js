f5b6b86857e137a818f4a043e4bbff71d2d975aa
carbontax <carbontax@gmail.com>
2013-04-29 17:58:48 -0400

    DB connections distinguished by process.env

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
@@ -10,13 +8,16 @@ var express = require('express')
   , path = require('path');
 var mongoose = require('mongoose');
 
-mongoose.set('debug', true);
-//mongoose.connect('mongodb://localhost/auctioneer');
-mongoose.connect('mongodb://nodejitsu:53a9a217c6407cd9ef88ec7ad2aa5af7@linus.mongohq.com:10060/nodejitsudb1239741365');
-
 var app = express();
+app.configure('development', function() {
+    mongoose.set('debug', true);
+    mongoose.connect('mongodb://localhost/auctioneer');
+});
 
-// all environments
+app.configure('production', function() {
+    mongoose.connect('mongodb://nodejitsu:53a9a217c6407cd9ef88ec7ad2aa5af7@linus.mongohq.com:10060/nodejitsudb1239741365');
+});
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -24,14 +25,15 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.basicAuth('carbontax','secret'));
+app.configure('production', function() {
+    app.use(express.basicAuth('carbontax','secret'));
+});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.configure('development', function() {
+    app.use(express.errorHandler());
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
@@ -41,5 +43,5 @@ app.put('/users', user.update);
 app.delete('/users/:username', user.deleteUser);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+   console.log('Express server listening on port ' + app.get('port'));
 });
